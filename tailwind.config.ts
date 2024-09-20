632aefd61da6335ef64c0f80b67ed39d26b101d7
Dawson Par <dawson@andrews.edu>
2024-09-20 14:04:51 +0900

    chore: defined light and dark theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,8 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primaryColor: "var(--primary-color)",
+        toggle: "var(--toggle)",
       },
     },
   },
