0ddce8b7b2b6c5ecacf1156bd7694708a6b6fcc4
Dawson Par <dawson@andrews.edu>
2024-09-20 14:24:17 +0900

    chore: moved viewport from metadata to viewport export

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,14 @@ import Home from "../components/home/Home";
 export const metadata = {
   title: "Dawson Par",
   description: "Messing around with NextJS",
-  viewport: "width=device-width, initial-scale=1",
   icons: {
     icon: "/favicon.ico",
   },
+};
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
 };
 
 export default function HomePage() {
