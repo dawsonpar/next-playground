f1e3646fc3e6eda87336e678096ea384465fce17
Dawson Par <dawson@andrews.edu>
2024-09-20 14:10:39 +0900

    chore: added metadata in root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
 import Home from "../components/home/Home";
 
-export default function home() {
+export const metadata = {
+  title: "Dawson Par",
+  description: "Messing around with NextJS",
+  viewport: "width=device-width, initial-scale=1",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+export default function HomePage() {
   return (
     <>
       <Home />
